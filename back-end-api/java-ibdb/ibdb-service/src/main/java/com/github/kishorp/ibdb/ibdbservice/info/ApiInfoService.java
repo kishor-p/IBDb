76049911a79bcdb3f3552f98300ceb310d232d95
Kishor Prakash <kishor-p@users.noreply.github.com>
2023-02-12 01:09:48 +0100

    Removed unnecessary code

--- back-end-api/java-ibdb/ibdb-service/src/main/java/com/github/kishorp/ibdb/ibdbservice/info/ApiInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.github.kishorp.ibdb.ibdbservice.info;
-
-import com.github.kishorp.ibdb.ibdbdomain.info.ApiInfo;
-
-public interface ApiInfoService {
-
-    ApiInfo getFullApiInfo();
-
-}
